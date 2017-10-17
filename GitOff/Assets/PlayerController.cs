cb95be8a3b41e36a2f0ca02f264d92417db5c37a
Jurmungandur <31280587+Jurmungandur@users.noreply.github.com>
2017-10-17 17:15:54 +0200

    Game v1.0

---
+++ GitOff/Assets/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float maxSpeed;
+	public float acc;
+	public float drag;
+	private float speed;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		if (Input.GetButton("Horizontal"))
+		{
+			if (speed < maxSpeed)
+			{
+				speed += acc;
+			}
+			else
+			{
+				speed = maxSpeed;
+			}
+		}
+		else if (Input.GetButton("Vertical"))
+		{
+			if (speed < maxSpeed)
+			{
+				speed += acc;
+			}
+			else
+			{
+				speed = maxSpeed;
+			}
+		}
+		else
+		{
+			speed *= drag;
+		}
+	}
+
+	void FixedUpdate()
+	{
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Time.fixedDeltaTime * speed);
+	}
+}
