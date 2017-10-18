02f547b0dd1be38e99ac99061c5ce73bbe2f7be5
Jurmungandur <31280587+Jurmungandur@users.noreply.github.com>
2017-10-18 17:29:28 +0200

    Game v1.3
    Inplumenting dimensions.

---
+++ GitOff/Assets/PlayerDimensionController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDimensionController : MonoBehaviour {
+
+    public int dimension = 0;
+    private int maxDimension = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Space"))
+        {
+            if (dimension < maxDimension)
+            {
+                transform.Translate(new Vector3(0, 100, 0));
+                dimension += 1;
+            }
+        }
+        if (Input.GetButtonDown("Shift"))
+        {
+            if (dimension > -maxDimension)
+            {
+                transform.Translate(new Vector3(0, -100, 0));
+                dimension -= 1;
+            }
+        }
+    }
+}
